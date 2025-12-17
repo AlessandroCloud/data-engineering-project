6363b91c750cc658cc49a025604e937b6732d0a3
AlessandroCloud <alessandro.dastolfo01@gmail.com>
2025-12-17 11:35:04 +0100

    StarSchema

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,36 +1,29 @@
 import streamlit as st
 import duckdb
-import pandas as pd
+import polars as pl
 
 st.set_page_config(page_title="F1 Dashboard", layout="wide")
 
 st.title("Formula 1 – Data Engineering Dashboard")
-st.write("Verifica connessione DuckDB e tabelle Bronze")
 
-# Connessione al DB 
 DB_PATH = "/data/warehouse.duckdb"
 
-try:
-    con = duckdb.connect(DB_PATH, read_only=True)
-    st.success(f"Connesso a DuckDB: {DB_PATH}")
-except Exception as e:
-    st.error("Errore di connessione a DuckDB")
-    st.exception(e)
-    st.stop()
 
-# Elenco tabelle
+con = duckdb.connect(DB_PATH, read_only=True)
+
+
 tables = con.execute("""
     SELECT table_schema, table_name
     FROM information_schema.tables
     ORDER BY table_schema, table_name
-""").fetchdf()
+""").pl()
 
 st.subheader("Tabelle presenti nel database")
-st.dataframe(tables, use_container_width=True)
+st.dataframe(tables)
 
 
 st.subheader("Anteprima gare (bronze.races)")
-df_races = con.execute("SELECT * FROM bronze.races LIMIT 20").fetchdf()
-st.dataframe(df_races, use_container_width=True)
+races = con.execute("SELECT * FROM bronze.races LIMIT 20").pl()
+st.dataframe(races)
 
 con.close()
