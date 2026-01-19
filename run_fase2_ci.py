0d84291a37248affe878ca2c092fa702cde5daba
AlessandroCloud <alessandro.dastolfo01@gmail.com>
2026-01-19 12:31:32 +0100

    run_fase2_ci

--- run_fase2_ci.py
+++ run_fase2_ci.py
@@ -1,4 +1,4 @@
-from etl.tasks.bronze_fase2 import load_bronze_f1_incremental
+﻿from etl.tasks.bronze_fase2 import load_bronze_f1_incremental
 from etl.tasks.silver_fase2 import build_silver_f1
 from etl.tasks.gold_fase2 import build_gold_f1
 
